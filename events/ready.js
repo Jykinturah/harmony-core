17fd71719709b3f8c41487850fd294002b6bebde
Jykinturah Sparkle <jykinturah@gmail.com>
2022-11-06 18:36:24 -0800

    Fix presence

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,10 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(client.user.tag + " online.");
-    client.user.setActivity("queries...",{type:"LISTENING"});
+    client.user.setPresence({
+      activities: [{ name: `queries...`, type: ActivityType.Listening}],
+      status: 'dnd'
+    });
     console.log("https://discordapp.com/oauth2/authorize?client_id="+client.user.id+"&scope=bot");
   }
 };

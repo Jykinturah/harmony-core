db41a19953361f49315f854217c30a46ede3efcf
Jykinturah <magpie@jyk.me>
2023-09-20 16:02:26 -0700

    Fix presence, update dependencies

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = {
   execute(client) {
     console.log(client.user.tag + " online.");
     client.user.setPresence({
-      activities: [{ name: `queries...`, type: ActivityType.Listening}],
+      activities: [{ name: `queries...`, type: 2}],
       status: 'dnd'
     });
     console.log("https://discordapp.com/oauth2/authorize?client_id="+client.user.id+"&scope=bot");
